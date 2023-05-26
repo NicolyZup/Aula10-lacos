c819573b0dc5c6edc7cb6b9de893f4b9e05dd173
NicolyZup <nicoly.vitorio@zup.com.br>
2023-05-26 15:26:30 -0300

    Criado pastas e iniciado exercicios da primeira lista

---
+++ Aula10/src/Lista1/Exercicio1.java
@@ -0,0 +1,11 @@
+//1 - Faça um programa que mostra na tela os números de 1 a 100.
+
+package Lista1;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=100; i++){
+            System.out.println(i);
+        }
+    }
+}
